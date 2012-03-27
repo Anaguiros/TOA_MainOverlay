4201f19abf20181aa30d13c9b75ecd450cd35c41
Anaguiros <matthieu.lenogue@gmail.com>
2012-03-28 00:45:24 +0200

    Debut de GUI en SWT. Me manque plus que les listener.

--- eclipse.project.Overlay/src/eclipse/project/overlay/views/MainViewPerso.java
+++ eclipse.project.Overlay/src/eclipse/project/overlay/views/MainViewPerso.java
@@ -4,13 +4,18 @@ import javax.swing.*;
 
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
@@ -26,48 +31,87 @@ public class MainViewPerso extends ViewPart{
 	public void createPartControl(final Composite parent) {
 		// TODO Auto-generated method stub
 		
-		Shell shell = parent.getShell();
+		final Shell shell = parent.getShell();
 		
-		GridLayout myGridLayout = new GridLayout(2,false);
-		parent.setLayout(myGridLayout);
+		FillLayout myFillLayout = new FillLayout(SWT.VERTICAL);
+		parent.setLayout(myFillLayout);
 		
-		Label videoSourceLabel = new Label(parent, SWT.NONE);
+		
+		
+		Group groupVideo = new Group(parent, SWT.NONE);
+		groupVideo.setLayout (new GridLayout(2, false) );
+		groupVideo.setText ("Capture Vidéo");
+				
+		Label videoSourceLabel = new Label(groupVideo, SWT.NONE);
 		videoSourceLabel.setText("Source vidéo : ");
 		
-//		final Text videoSourceValue = new Text(parent, SWT.BORDER);
-//		GridData myGridData = new GridData(GridData.FILL_HORIZONTAL);
-//		videoSourceValue.setLayoutData(myGridData);
-		
-		final Combo videoSourceValue = new Combo(parent, SWT.NONE);
+		final Combo videoSourceValue = new Combo(groupVideo, SWT.READ_ONLY);
 		videoSourceValue.add("Fichier");
 		videoSourceValue.add("Webcam");
 		videoSourceValue.add("URL");
 		GridData myGridData = new GridData(GridData.FILL_HORIZONTAL);
-		videoSourceValue.setLayoutData(myGridData);
+		//videoSourceValue.setLayoutData(myGridData);
+	    
+	    final Text sourceName = new Text(groupVideo, SWT.BORDER);
+	    sourceName.setLayoutData(myGridData);
 		
+	    Button open = new Button(groupVideo, SWT.PUSH);
+	    open.setText("Open...");
+	    open.addSelectionListener(new SelectionAdapter() {
+	    	public void widgetSelected(SelectionEvent event) {
+	    		// User has selected to open a single file
+	    		FileDialog dlg = new FileDialog(shell, SWT.OPEN);
+	    		dlg.setFilterExtensions(new String[] { "*.avi", "*.mov", "*.*" });
+//	    		dlg.setFilterNames(FILTER_NAMES);
+//	    		dlg.setFilterExtensions(FILTER_EXTS);
+	    		String fn = dlg.open();
+	    		if (fn != null) {
+					sourceName.setText(fn);
+	    		}
+	    	}
+	    });
+	    
+
 		
-		Label companyNameLabel = new Label(parent, SWT.NONE);
-		companyNameLabel.setText("Société : ");
+		Group groupSocial = new Group(parent, SWT.NONE);
+		groupSocial.setLayout (new GridLayout(2, false) );
+		groupSocial.setText ("Réseau Social");
 		
-		final Text companyNameValue = new Text(parent, SWT.BORDER);
+		Label networkTypeLabel = new Label(groupSocial, SWT.NONE);
+		networkTypeLabel.setText("Réseau : ");
+		
+		final Combo networkTypeValue = new Combo(groupSocial, SWT.READ_ONLY);
+		networkTypeValue.add("Facebook");
+		networkTypeValue.add("Twitter");
+		networkTypeValue.add("Blogger");
 		myGridData = new GridData(GridData.FILL_HORIZONTAL);
-		companyNameValue.setLayoutData(myGridData);
+		networkTypeValue.setLayoutData(myGridData);
+
+		Group groupControl = new Group(parent, SWT.NONE);
+		groupControl.setLayout (new GridLayout(2, false) );
+		//groupControl.setText ("Capture Vidéo");
 		
-		JButton newAttendee = new JButton();
-		newAttendee.setText("Ajouter");		
-		newAttendee.addSelectionListener(new SelectionListener() {			
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				MessageDialog.openInformation(
-						parent.getShell(),
-						"Vue principale",
-						"cliquage de bouton");
-			}
-			
-			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {		
-			}
-		});
+		Button acceptButton = new Button(groupControl, SWT.PUSH);
+		acceptButton.setText("Accept");
+		
+		Button cancelButton = new Button(groupControl, SWT.PUSH);
+		cancelButton.setText("Cancel");
+		
+//		Button newAttendee = new Button(parent, SWT.BORDER);
+//		newAttendee.setText("Ajouter");		
+//		newAttendee.addSelectionListener(new SelectionListener() {			
+//			@Override
+//			public void widgetSelected(SelectionEvent e) {
+//				MessageDialog.openInformation(
+//						parent.getShell(),
+//						"Vue principale",
+//						"cliquage de bouton");
+//			}
+//			
+//			@Override
+//			public void widgetDefaultSelected(SelectionEvent e) {		
+//			}
+//		});
 	}
 
 	@Override
